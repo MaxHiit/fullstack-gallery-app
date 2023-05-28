408505b0fcc043bb30a4a6e22bcb8920e14150b0
MaxHiit <pro.maxcode@gmail.com>
2023-05-28 23:33:33 +0200

    feat(style | package): add date package | set styling configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ['localhost']
+	},
+	env: {
+		API_URL_BASE: process.env.API_URL_BASE
+	}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

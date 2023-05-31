886aebc78ba217271f952b53d80a063db92ce2c9
MaxHiit <pro.maxcode@gmail.com>
2023-05-31 04:41:05 +0200

    feat(component): generic button

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -2,11 +2,17 @@ import React, { ComponentPropsWithoutRef } from 'react';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 export const button = tv({
-	base: 'border-none text-white rounded-md px-4 py-2 flex gap-2 items-center justify-center',
+	base:
+		'text-white rounded-md px-4 py-2 flex gap-2 items-center justify-center transition-all duration-300 ease-in-out',
 	variants: {
 		color: {
-			primary: 'bg-blue-500 text-white',
-			secondary: 'bg-gray-500 text-white'
+			primary: 'border-none hover:bg-blue-800 bg-blue-500 text-white',
+			secondary: 'bg-gray-500 text-white',
+			icon:
+				'w-fit p-1 text-slate-300 transition-all duration-300 ease-in-out group-hover:text-slate-300'
+		},
+		isTransparent: {
+			true: 'bg-none'
 		}
 	}
 });
@@ -16,12 +22,19 @@ type ButtonType = ComponentPropsWithoutRef<'button'>;
 
 interface ButtonProps extends Omit<ButtonType, 'color'>, ButtonVariants {
 	'aria-label': string;
+	className?: string;
 	children: React.ReactNode;
 }
 
-const Button = ({ children, ...rest }: ButtonProps) => {
+const Button = ({
+	color = 'primary',
+	isTransparent,
+	children,
+	className,
+	...rest
+}: ButtonProps) => {
 	return (
-		<button className={button({ color: 'primary' })} {...rest}>
+		<button className={button({ color, isTransparent, className })} {...rest}>
 			{children}
 		</button>
 	);
--- app/components/input/Input.tsx
+++ app/components/input/Input.tsx
@@ -3,11 +3,14 @@ import { VariantProps, tv } from 'tailwind-variants';
 
 export const input = tv({
 	base:
-		'w-full bg-transparent border-2 border-solid border-blue-500 rounded-md px-4 py-2 text-black placeholder:text-gray-light hover:border-blue-800',
+		'w-full bg-transparent border-2 border-solid border-blue-500 rounded-md px-4 py-2 text-black placeholder:text-gray-light hover:border-blue-800 transition-all duration-300 ease-in-out',
 	variants: {
 		filevariant: {
 			true:
-				'p-0 file:h-full file:cursor-pointer hover:file:bg-blue-800 file:px-4 file:py-2 file:bg-blue-500 file:text-white file:border-none file:me-4'
+				'p-0 file:h-full file:cursor-pointer group-hover:file:bg-blue-800 file:px-4 file:py-2 file:bg-blue-500 file:text-white file:border-none file:me-4'
+		},
+		rangeVariant: {
+			true: 'w-ful h-2 bg-gray-200 border-none p-0 rounded-lg appearance-none cursor-pointer'
 		}
 	}
 });
@@ -22,7 +25,15 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement>, InputVariant
 }
 
 const Input = forwardRef(function Input(
-	{ filevariant = false, label, name, placeholder, className, ...rest }: InputProps,
+	{
+		filevariant = false,
+		rangeVariant = false,
+		label,
+		name,
+		placeholder,
+		className,
+		...rest
+	}: InputProps,
 	ref: ForwardedRef<HTMLInputElement>
 ) {
 	return (
@@ -33,7 +44,7 @@ const Input = forwardRef(function Input(
 				</label>
 			)}
 			<input
-				className={input({ filevariant: filevariant })}
+				className={input({ filevariant: filevariant, rangeVariant })}
 				name={name}
 				ref={ref}
 				{...rest}
